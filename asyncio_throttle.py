f0e43b36b12e05c467addf87e00c13cc8da8be39
Alexey Kolyanov <alexey.kolyanov@gmail.com>
2018-02-21 23:56:06 +0300

    minor fixes, added ThrottlerPool

--- asyncio_throttle.py
+++ asyncio_throttle.py
@@ -1,12 +1,16 @@
+
 import time
 import asyncio
 from collections import deque
+from itertools import cycle
 
-class Throttler:
-    def __init__(self, rate_limit, period=1.0, retry_interval=0.01):
+
+class Throttler(object):
+
+    def __init__(self, rate_limit, period=1.0, name=None):
         self.rate_limit = rate_limit
         self.period = period
-        self.retry_interval = retry_interval
+        self.name = name
 
         self._task_logs = deque()
 
@@ -18,17 +22,65 @@ class Throttler:
             else:
                 break
 
-    async def acquire(self):
+    def is_ready(self):
+        self.flush()
+        return len(self._task_logs) < self.rate_limit
+
+    async def acquire(self, wait=True):
         while True:
-            self.flush()
-            if len(self._task_logs) < self.rate_limit:
+            if self.is_ready():
                 break
-            await asyncio.sleep(self.retry_interval)
+
+            if not wait:
+                return False
+
+            sleeptime = self.period - (time.time() - self._task_logs[0])
+            if sleeptime > 0:
+                await asyncio.sleep(sleeptime)
 
         self._task_logs.append(time.time())
+        return True
 
     async def __aenter__(self):
-        await self.acquire()
+        await self.acquire(wait=True)
 
     async def __aexit__(self, exc_type, exc, tb):
-        pass
+        await asyncio.sleep(0)
+
+
+class ThrottlerPool(object):
+    """
+        Might be useful to serve multiple homogeniuos
+            resources (e.g server pool) with same rate limit
+    """
+
+    def __init__(self, rate_limit, period=1.0,
+                 names=None, retry_interval=0.01):
+        self._pool = []
+        self.rate_limit = rate_limit
+        self.period = period
+        self.retry_interval = retry_interval
+        names = names or []
+        for name in names:
+            self._pool.append(Throttler(rate_limit=rate_limit,
+                                        period=period,
+                                        name=name))
+
+    def cyclepool(self):
+        # balancing the load
+        for thr in cycle(self._pool):
+            yield thr
+
+    async def acquire(self):
+        while True:
+            for thr in self.cyclepool():
+                if await thr.acquire(wait=False):
+                    return thr.name
+
+            await asyncio.sleep(self.retry_interval)
+
+    async def __aenter__(self):
+        return await self.acquire()
+
+    async def __aexit__(self, exc_type, exc, tb):
+        await asyncio.sleep(0)
